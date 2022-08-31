09da7192e9fe5ae6ac6900441f41c094826cbf34
itsxJuice <112575836+itsxJuice@users.noreply.github.com>
2022-09-01 03:03:24 +0600

    Add files via upload

---
+++ js/godisagirl.js
@@ -0,0 +1,9 @@
+	var r_text = new Array ();
+			r_text[0] = "no one escapes the punishment of heaven";
+			r_text[1] = "god is a girl";
+			r_text[2] = "the day will come when the sky will turn bloody";
+			r_text[3] = "very soon the angels will descend from heaven";
+			r_text[4] = "gl hf";
+			r_text[5] = "the end is near";
+			var i = Math.floor(6*Math.random())
+			document.write(r_text[i]);
